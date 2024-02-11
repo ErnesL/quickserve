90bdfadbd7e110705b3e5c45cf68da2dcb37daf3
ErnesL <ernesto.lugo@correo.unimet.edu.ve>
2024-02-11 17:59:44 -0400

    model food fixed

--- models/Food.js
+++ models/Food.js
@@ -1,18 +1,29 @@
 import mongoose from "mongoose";
 
-const FoodSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    require: [true, "Please, input the food's name"],
+const FoodSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: [true, "Please, input the food's name"],
+    },
+    description: {
+      type: String,
+      require: [true, "Please, input the food's description"],
+    },
+    ingredients: {
+      type: String,
+      require: [true, "Please, input the food's ingredients"],
+    },
+    cost: {
+      type: Number,
+      require: [true, "Please, input the food's cost"],
+    },
+    processed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  description: {
-    type: String,
-    require: [true, "Please, input the food's description"],
-  },
-  ingredients: {
-    type: String,
-    require: [true, "Please, input the food's ingredients"],
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.Food || mongoose.model("Food", FoodSchema);
