35d9d53b55f4c9da05fa477e8ae79dcfe00a00c6
Buga <juandbugallo@gmail.com>
2024-02-11 22:20:05 -0400

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -83,7 +83,16 @@ export default function NavBar() {
                 Contacto
               </a>
             </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+              Carrito <img src = "quickserve/assets/img/cart-shopping-fast.png" alt = "" className="w-50 h-50 mr-20" />
+              </a>
+            </li>
           </ul>
+          
         </div>
       </div>
     </nav>
