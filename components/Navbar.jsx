4ad1c60cc8c18f40cfb07f3e984759f8fb3c0e39
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2024-02-13 15:13:16 -0400

    Avances en FoodList

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -39,7 +39,7 @@ export default function NavBar() {
           </svg>
         </button>
         <div
-          className="hidden w-full md:block md:w-auto grid justify-items-end"
+          className="w-full md:block md:w-auto grid justify-items-end"
           id="navbar-default"
         >
           <ul className="font-medium items-center flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
