a8fe637da110c072ad17908dc194e8cfd6267eaf
Radenixlol <ranseartin@gmail.com>
2024-02-12 23:52:53 -0400

    Añadir comidas

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -77,12 +77,14 @@ export default function NavBar() {
               </a>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contacto
-              </a>
+              <Link href={"/addFood"}
+                className="block py-2 px-3 text-gray-900 rounded
+                hover:bg-gray-100 md:hover:bg-transparent md:border-0
+                md:hover:text-blue-700 md:p-0 dark:text-white
+                md:dark:hover:text-blue-500 dark:hover:bg-gray-700
+                dark:hover:text-white md:dark:hover:bg-transparent" >
+                Añadir
+              </Link>
             </li>
             <li>
               <Image
