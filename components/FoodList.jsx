a6a35ad62e05607252a9730d2634927a46a5c0c7
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2024-02-13 11:44:52 -0400

    Modificaciones

--- components/FoodList.jsx
+++ components/FoodList.jsx
@@ -22,17 +22,25 @@ export default async function FoodList() {
     <>
       {foods.map((f) => (
         <div key={f}>
-          <a
-            href="#"
-            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
-          >
+          <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {f.title}
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              {f.description}
-            </p>
-          </a>
+            <div className="grid gap-4 grid-cols-2 grid-rows-1">
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                {f.description}
+              </p>
+              <div className="grid gap-4 grid-cols-3 grid-rows-1">
+                <button className="btn btn-circle btn-outline">
+                  <h5 className="text-white">-</h5>
+                </button>
+                <h5>0</h5>
+                <button className="btn btn-circle btn-outline">
+                  <h5 className="text-white">+</h5>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </>
