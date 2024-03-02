66436c3a8aa32735d413e6b108dc6285c31cede8
Andres Bucarello <andres.bucarello@correo.unimet.edu.ve>
2024-03-02 14:24:34 -0400

    Componente Card

---
+++ components/Card.jsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Card({
+  image,
+  title,
+  ingredients,
+  description,
+  price,
+  quantity,
+  onDecrement,
+  onIncrement,
+}) {
+  return (
+    <>
+      <div className="p-3">
+        <div className="flex flex-col justify-between content-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 min-w-80 min-h-80">
+          <div className="min-w-80 min-h-80 bg-green-50 p-1">{image}</div>
+
+          <div className="self-center p-4">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {title}
+            </h5>
+          </div>
+
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            {ingredients}
+          </p>
+
+          <div className="self-center p-2">
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {description}
+            </p>
+          </div>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            ${price}
+          </p>
+
+          <div className="flex self-center justify-center space-x-4 items-center p-4">
+            <button
+              className="btn bg-white p-3 rounded-3xl"
+              onClick={onDecrement}
+            >
+              <h1>-</h1>
+            </button>
+
+            <p className="text-white">{quantity}</p>
+
+            <button
+              className="btn bg-white p-3 rounded-3xl"
+              onClick={onIncrement}
+            >
+              <h1>+</h1>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- components/FoodList.jsx
+++ components/FoodList.jsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Card from "@/components/Card";
+import { CircularProgress } from "@material-ui/core";
 
 const getFoods = async () => {
   try {
@@ -89,56 +91,32 @@ export default function FoodList() {
 
   return (
     <>
+      import Card from "./Card";
+
       <div className="flex flex-wrap p-2 justify-center pt-20">
         {Array.isArray(foods) ? (
           foods.map((food, index) => (
             <div className="p-3" key={food.id || index}>
-              <div className="flex flex-col justify-between content-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 min-w-80 min-h-80">
-                <div className="min-w-80 min-h-80 bg-green-50 p-1">
-                  <h1> IMAGEN</h1>
-                </div>
-
-                <div className="self-center p-4">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {food.title}
-                  </h5>
-                </div>
-
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  {food.ingredients}
-                </p>
-
-                <div className="self-center p-2">
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {food.description}
-                  </p>
-                </div>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  ${food.price}
-                </p>
-
-                <div className="flex self-center justify-center space-x-4 items-center p-4">
-                  <button
-                    className="btn bg-white p-3 rounded-3xl"
-                    onClick={() => decrementQuantity(index)}
-                  >
-                    <h1>-</h1>
-                  </button>
-
-                  <p className="text-white">{quantities[index]}</p>
-
-                  <button
-                    className="btn bg-white p-3 rounded-3xl"
-                    onClick={() => incrementQuantity(index)}
-                  >
-                    <h1>+</h1>
-                  </button>
-                </div>
-              </div>
+              <Card
+                image={<h1>IMAGEN</h1>}
+                title={food.title}
+                ingredients={food.ingredients}
+                description={food.description}
+                price={food.price}
+                quantity={quantities[index]}
+                onDecrement={() => decrementQuantity(index)}
+                onIncrement={() => incrementQuantity(index)}
+              />
             </div>
           ))
         ) : (
-          <p>Cargando alimentos...</p>
+          <p>
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              "Cargando alimentos..."
+            )}
+          </p>
         )}
       </div>
       <br />
