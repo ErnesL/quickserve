992faf055caf9a1c172d2bfb3d258b7179d9df38
Juan David <juandbugallo@gmail.com>
2024-03-02 18:15:52 -0400

    Nuevas Cards y Botones

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,61 +1,39 @@
-"use client";
-
 import Link from "next/link";
 
 export default function Card({
   image,
   title,
-  ingredients,
   description,
   price,
   quantity,
   onDecrement,
   onIncrement,
 }) {
+  const defaultImage = "https://catering.grupoelescondite.com/wp-content/uploads/2020/07/menu-degustacion.jpg";
+
+  const renderImage = typeof image === 'string' ? (
+    <img src={image || defaultImage} alt={title || "Plate"} />
+  ) : image;
+
   return (
-    <>
-      <div className="p-3">
-        <div className="flex flex-col justify-between content-center max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 min-w-80 min-h-80">
-          <div className="min-w-80 min-h-80 bg-green-50 p-1">{image}</div>
-
-          <div className="self-center p-4">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {title}
-            </h5>
-          </div>
-
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {ingredients}
-          </p>
-
-          <div className="self-center p-2">
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              {description}
-            </p>
-          </div>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            ${price}
-          </p>
-
-          <div className="flex self-center justify-center space-x-4 items-center p-4">
-            <button
-              className="btn bg-white p-3 rounded-3xl"
-              onClick={onDecrement}
-            >
-              <h1>-</h1>
-            </button>
-
-            <p className="text-white">{quantity}</p>
-
-            <button
-              className="btn bg-white p-3 rounded-3xl"
-              onClick={onIncrement}
-            >
-              <h1>+</h1>
-            </button>
-          </div>
+    <div className="card card-compact w-96 bg-black shadow-xls rounded">
+      <figure>
+        <img src={defaultImage} alt={title || "Plate"} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-white">{title || "Title"}</h2>
+        <p className="text-white">{description || "Description"}</p>
+        <p className="text-white">${price || 0}</p>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} className="card-actions">
+          <button className="btn btn-circle text-white" onClick={onDecrement}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 12H4" /></svg>
+          </button>
+          <p className="text-white text-center">{quantity}</p>
+          <button className="btn btn-circle btn-outline text-white" onClick={onIncrement}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v12M6 12h12" /></svg>
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
