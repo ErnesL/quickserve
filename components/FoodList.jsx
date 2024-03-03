d1aa8e3b4c8f34eec9304dcfd820a3a228b2ce7e
Andres Bucarello <andres.bucarello@correo.unimet.edu.ve>
2024-03-03 15:46:01 -0400

    Mejoras
    Creacion del componente Drawer.jsx
    
    Mejora del codigo de FoodList.jsx con la implementacion de drawer
    
    Mejora de las tarjetas en el preview del carrito

--- components/FoodList.jsx
+++ components/FoodList.jsx
@@ -1,10 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import CardPreview from "@/components/CardPreview";
 import Card from "@/components/Card";
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
+import Drawer from "@/components/Drawer";
+
 
 const getFoods = async () => {
   try {
@@ -157,90 +156,23 @@ export default function FoodList() {
       {/* drawer */}
       <div className="flex justify-end mr-[10vw] mt-[5vh]">
         {["right"].map((anchor) => (
-          <React.Fragment key={anchor}>
-            <Button onClick={toggleDrawer(anchor, true)}>
-              Preview del carrito
-            </Button>
-            <Drawer
-              anchor={anchor}
-              open={state[anchor]}
-              onClose={toggleDrawer(anchor, false)}
-            >
-              {list(anchor)}
-              <div className="flex">
-                <div className="min-w-[30vw] max-w-[50vw] grid grid-cols-1 p-3">
-                  <h4 className="mb-2 justify-self-center text-2xl font-bold tracking-tight text-gray-900 dark:text-black p-4">
-                    Carrito
-                  </h4>
-                  <ul>
-                    {/* Se mapea el arreglo filteredCart de forma que solo seleccione las comidas y no los nulls. Si es null, entonces no muestra nada */}
-                    {filteredCart.map((food, index) =>
-                      food != null ? (
-                        <li key={index}>
-                          <CardPreview
-                            image={<h1>IMAGEN</h1>}
-                            title={food.title}
-                            ingredients={food.ingredients}
-                            description={food.description}
-                            price={food.price}
-                            quantity={quantities[index]}
-                            onDecrement={() => decrementQuantity(index)}
-                            onIncrement={() => incrementQuantity(index)}
-                          />
-                        </li>
-                      ) : null
-                    )}
-
-                    <div className="p-3 flex justify-center">
-                      <form action="">
-                        {/* 
-                        <input id="GET-notes" className="rounded-full" type="text" name="name" /> */}
-                        <label className="font-normal text-gray-700 dark:text-gray-400 p-1">
-                          Notas de la orden <br /> <br />
-                        </label>
-                        <input
-                          className="border-b border-black"
-                          type="text"
-                          value={texto}
-                          onChange={handleChange}
-                        />
-                      </form>
-                    </div>
-                  </ul>
-                  <h4 className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-black p-4">
-                    Factura
-                  </h4>
-                  <br />
-                  <div className="p-2 ml-5">
-                    <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-500">
-                      Subtotal de la orden: ${subtotal} <br />
-                    </h6>
-                  </div>
-                  <div className="p-2 ml-5">
-                    <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-500">
-                      Taxes: ${taxes} <br />
-                    </h6>
-                  </div>
-                  <div className="p-2 ml-5">
-                    <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-500">
-                      Total: ${total}
-                    </h6>
-                  </div>
-                  //#TODO: 004
-                  <button
-                    className="btn btn-outline btn-success"
-                    onClick={() => {
-                      {
-                        writeCartToMongoDB(filteredCart);
-                      }
-                    }}
-                  >
-                    Pagar
-                  </button>
-                </div>
-              </div>
-            </Drawer>
-          </React.Fragment>
+          <Drawer
+          key={anchor}
+          anchor={anchor}
+          state={state}
+          toggleDrawer={toggleDrawer}
+          list={list}
+          filteredCart={filteredCart}
+          quantities={quantities}
+          decrementQuantity={decrementQuantity}
+          incrementQuantity={incrementQuantity}
+          texto={texto}
+          handleChange={handleChange}
+          subtotal={subtotal}
+          taxes={taxes}
+          total={total}
+          writeCartToMongoDB={writeCartToMongoDB}
+        />
         ))}
       </div>
 
@@ -266,7 +198,7 @@ export default function FoodList() {
             </div>
           ))
         ) : (
-          <p>{loading ? <CircularProgress /> : "Cargando alimentos..."}</p>
+          <p>Cargando alimentos...</p>
         )}
       </div>
       <br />
